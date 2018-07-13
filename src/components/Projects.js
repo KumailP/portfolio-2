7b01f30e33a801706bc5e049da812d49973115ef
KumailP <kumailpirzada@gmail.com>
2018-07-14 03:11:05 +0500

    Updated projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,92 @@
+import React from "react";
+import "./styles/Projects.css";
+
+export default () => {
+  return (
+    <div className="projects">
+      <h1>Projects</h1>
+      <div className="projects-list">
+        <div
+          className="project"
+          style={{
+            backgroundImage: "url('/images/projects/007.png')"
+          }}
+        >
+          <div className="project-text">
+            <h2>Goldeneye World Records</h2>
+            <p>A web app that</p>
+            <div className="project-tools">
+              <span>ReactJS</span>
+              <span>NodeJS</span>
+              <span>ExpressJS</span>
+            </div>
+            <div className="project-buttons">
+              <button type="button">Live Demo</button>
+              <button type="button">GitHub</button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="project"
+          style={{
+            backgroundImage: "url('/images/projects/007.png')"
+          }}
+        >
+          <div className="project-text">
+            <h2>Goldeneye World Records</h2>
+            <p>A web app that</p>
+            <div className="project-tools">
+              <span>ReactJS</span>
+              <span>NodeJS</span>
+              <span>ExpressJS</span>
+            </div>
+            <div className="project-buttons">
+              <button type="button">Live Demo</button>
+              <button type="button">GitHub</button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="project"
+          style={{
+            backgroundImage: "url('/images/projects/007.png')"
+          }}
+        >
+          <div className="project-text">
+            <h2>Goldeneye World Records</h2>
+            <p>A web app that</p>
+            <div className="project-tools">
+              <span>ReactJS</span>
+              <span>NodeJS</span>
+              <span>ExpressJS</span>
+            </div>
+            <div className="project-buttons">
+              <button type="button">Live Demo</button>
+              <button type="button">GitHub</button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="project"
+          style={{
+            backgroundImage: "url('/images/projects/007.png')"
+          }}
+        >
+          <div className="project-text">
+            <h2>Goldeneye World Records</h2>
+            <p>A web app that</p>
+            <div className="project-tools">
+              <span>ReactJS</span>
+              <span>NodeJS</span>
+              <span>ExpressJS</span>
+            </div>
+            <div className="project-buttons">
+              <button type="button">Live Demo</button>
+              <button type="button">GitHub</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
